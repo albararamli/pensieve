215c0de6b1e6b1879d58c67ff3818574ca118d37
Albara Ramli <arramli@ucdavis.edu>
2019-04-25 09:46:23 -0700

    the local ip place

--- run_exp/run_all_traces.py
+++ run_exp/run_all_traces.py
@@ -13,9 +13,10 @@ with open('./chrome_retry_log', 'wb') as f:
 os.system('sudo sysctl -w net.ipv4.ip_forward=1')
 
 #ip_data = json.loads(urllib.urlopen("http://ip.jsontest.com/").read())
-ip_data = json.loads(urllib.urlopen("http://54.67.17.124/albara.ramli.net/researches/pensieve/ip.php?op=albara").read()) 
-
-ip = str(ip_data['ip'])
+#ip_data = json.loads(urllib.urlopen("http://54.67.17.124/albara.ramli.net/researches/pensieve/ip.php?op=albara").read()) 
+#ip = str(ip_data['ip'])
+fff=open("../../ip.txt","r")
+ip=fff.read()
 
 ABR_ALGO = 'BB'
 PROCESS_ID = 0
