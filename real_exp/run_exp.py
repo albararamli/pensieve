254f2f7c58979cd6052e1d9bad6f734b4a00695f
Albara Ramli <arramli@ucdavis.edu>
2019-04-29 15:14:44 -0700

    fix algo name

--- real_exp/run_exp.py
+++ real_exp/run_exp.py
@@ -7,7 +7,7 @@ import numpy as np
 RUN_SCRIPT = 'run_video.py'
 RANDOM_SEED = 42
 RUN_TIME = 280  # sec
-ABR_ALGO = ['fastMPC', 'robustMPC', 'BOLA', 'RL']
+ABR_ALGO = ['BB'] #'fastMPC', 'robustMPC', 'BOLA', 'RL']
 REPEAT_TIME = 10
 
 
