3a98b218292e8d587e53811e4179c5026f3cb479
Albara Ramli <arramli@ucdavis.edu>
2019-07-17 21:48:00 -0700

    Update run_exp.py

--- real_exp/run_exp.py
+++ real_exp/run_exp.py
@@ -3,13 +3,15 @@ import os
 import subprocess
 import numpy as np
 
+import time, subprocess
+from time import sleep
 
 RUN_SCRIPT = 'run_video.py'
 RANDOM_SEED = 42
-RUN_TIME = 210 #280  # sec
+RUN_TIME = 280 #280  # sec
 ABR_ALGO = [ 'RL','BB','FIXED','robustMPC', 'fastMPC', 'BOLA',  'FESTIVE','RB']
 #  [ 'RL','BB','FIXED','robustMPC', 'fastMPC', 'BOLA',  'FESTIVE','RB']
-REPEAT_TIME = 1
+REPEAT_TIME = 10
 
 
 def main():
@@ -25,7 +27,16 @@ def main():
 			for abr_algo in ABR_ALGO:
 
 				while True:
-
+					###################################
+					current_path=os.getcwd()
+					new_path="../../pantheon/"
+					os.chdir(new_path)
+					os.system("touch ./data/DONE.txt")
+					time.sleep(6)
+					ttt='"python third_party/indigo/env/proxy.py 9999 LINUX;exit;exec bash"'
+					os.system("gnome-terminal -e 'sh -c "+ttt+"'")
+					os.chdir(current_path)
+					##################################
 					script = 'python ' + RUN_SCRIPT + ' ' + \
 							  abr_algo + ' ' + str(RUN_TIME) + ' ' + str(rt)
 					print(script)
@@ -43,7 +54,15 @@ def main():
 						log.write(abr_algo + '_' + str(rt) + '\n')
 						log.write(out + '\n')
 						log.flush()
-
+				###################################
+				current_path=os.getcwd()
+				new_path="../../pantheon/"
+				os.chdir(new_path)
+				os.system("touch ./data/DONE.txt")
+				time.sleep(6)
+				os.chdir(current_path)
+				##################################
+					
 
 
 if __name__ == '__main__':
